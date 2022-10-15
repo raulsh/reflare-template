2a42bd3b296f0e96293b312c2b7fffddcb20fa54
Raul Salamanca <ralsalamanca@gmail.com>
2022-10-15 00:59:27 -0300

    update domain

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const handleRequest = async (
   reflare.push({
     path: '/*',
     upstream: {
-      domain: 'ifconfig.me',
+      domain: 'cej.pj.gob.pe',
       protocol: 'https',
     },
   });
